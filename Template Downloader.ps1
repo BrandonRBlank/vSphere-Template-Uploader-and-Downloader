ca254dadd589d242ebea789903d55dc46ca95b0d
BrandonRBlank <brandon_blank_r@yahoo.com>
2018-09-19 18:04:53 -0400

    Add files via upload

---
+++ Template Downloader.ps1
@@ -0,0 +1,67 @@
+﻿# CONFIG TO YOUR NEEDS-------------------------------------------------
+$VMDestination = ""  # Local dir to download templates to
+$VMFolder = ""       # Dir in vSphere to download templates from
+$RPool = ""          # Resource Pool your folder vsphere folder is in
+$Username = ""       # vSphere username
+$Password = ""       # vSphere user password
+$IP = ""             # vSphere server IP
+# ---------------------------------------------------------------------
+
+$VM = ""
+$StrayVMs = @() # Array to keep track of stray VMs in folder (Does not download)
+$RunFlag = 0
+$ConvertFlag = 1
+ 
+# Server login
+Connect-VIServer $IP -User $Username -Password $Password
+
+# Convert folder of templates to VMs (keeps track of present VMs; doesn't download)
+if ($ConvertFlag) {
+    Get-Folder $VMFolder | Get-VM |
+        ForEach-Object {
+            $StrayVMs += $_.Name
+        }
+    Get-Folder $VMFolder | Get-Template |
+        ForEach-Object {
+            Set-Template -Template $_.Name -ToVM -Confirm:$false
+        }
+}
+
+# Bulk Download from a specific folder (must be VMs, templates are not exportable)
+if ($VMFolder) {
+    Get-Folder $VMFolder | Get-VM |
+        ForEach-Object {
+            if ($StrayVMs -notcontains $_.Name) {
+                Export-VApp -Destination $VMDestination -VM $_.Name -CreateSeparateFolder -Force
+            }
+        }
+    $RunFlag = 1
+}
+
+# Single VM Download
+if ($VM) {   
+    Export-VApp -Destination $VMDestination -VM $VM -Force
+    $RunFlag = 1
+}
+
+# Converts VMs back to templates
+if ($ConvertFlag -eq 1) {
+    Get-Folder $VMFolder | Get-VM |
+        ForEach-Object {
+            if ($StrayVMs -notcontains $_.Name) {
+                Set-VM -VM $_.Name -ToTemplate -Confirm:$false
+            }
+        }
+}
+
+# Renames *.ovf to descriptor.ovf
+if ($RunFlag -eq 1) {
+    Get-ChildItem $VMDestination -Recurse -Filter *.ovf | 
+        ForEach-Object {
+            Rename-Item $_.FullName -NewName "template.ovf"
+        }
+}
+
+else {
+    "You need to specify a folder or single VM to export in file"
+}
---
+++ Template Uploader.ps1
@@ -0,0 +1,67 @@
+﻿# CHANGE TO YOUR NEEDS ---------------------------------
+$TemplatePATH = ""     # Local path to VM templates
+$VMFolder = ""         # Folder to upload VMs to
+$Format = "Thin"       # Template provisioning type
+$Username = ""         # vSphere username
+$Password = ""         # vSphere user password
+$IP = ""               # vSphere server IP
+# ------------------------------------------------------
+
+$VMHostArray = @() # Leave empty
+$VMLocArray = @() # Leave empty
+$HostCount = 0
+$GoFlag = 0
+
+# Server Login
+Connect-VIServer $IP -User $Username -Password $Password
+
+# Specify a network to map to (Commented out by default)
+# You'll need to add "-OvfConfiguration $ovfconfig" to Import -vApp perameters
+#$ovfconfig.NetworkMapping.bridged.Value = ""
+
+# Gets all VM Host servers and adds them to an array
+ForEach($VM in Get-VMHost){
+    if ($VM.Name -like "crdevesx*") {
+        $VMHostArray += $VM.Name
+    }
+}
+$NumVMHosts = $VMHostArray.count
+$HostMAX = $NumVMHosts - 1
+
+# Gets all Clusters and add thems to an array
+$LocGet = Get-Cluster
+ForEach($Loc in $LocGet){
+    $VMLocArray += $Loc.Name
+}
+
+$VMfolder = Get-Inventory -Name $VMFolder
+
+# For VSphere
+Get-ChildItem $TemplatePATH -Recurse | 
+    ForEach-Object {
+        if ($_.Name -match ".mf") {
+            $VMName = $_.Name.TrimEnd(".mf")
+            $VMTemplate = $_.FullName.TrimEnd($_.Name) + "template.ovf"
+            $GoFlag = 1
+        }
+
+        $VMHost = $VMHostArray[$HostCount]
+        if ($HostCount -eq $HostMAX) {
+            $HostCount = 0
+        }
+        else {
+            $HostCount++
+        }
+
+        # Imports Template
+        if ($GoFlag -eq 1) {
+            Import-VApp -Source $VMTemplate -VMHost $VMHost -Name $VMName -Force -DiskStorageFormat $Format
+
+            $VMList = Get-VM -Name $VMName
+
+            # Moves VM to designated folder (Host/Cluster and VM Folder)
+            Move-VM -VM $VMList.Name -InventoryLocation $VMFolder
+
+        }
+        $GoFlag = 0
+}
